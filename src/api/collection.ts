7feafb0cf894e7e09073b0fa9fdee704a6636c6c
DESKTOP-FJ2SE9R\PC <ammarabror23@gmail.com>
2024-09-13 09:29:57 +0700

    update

--- src/api/collection.ts
+++ src/api/collection.ts
@@ -4,9 +4,9 @@ import moment from "moment";
 export async function getDataUndian() {
   const query = {
     query: `query {
-        transactions(userId: null, mobile: null, isWinner: "false", uniqueCode: null, branch: null, startAt: ${moment().format(
+        transactions(userId: null, mobile: null, isWinner: "false", uniqueCode: null, branch: null, startAt: "${moment().format(
           "YYYY-MM-DD"
-        )}, endAt: ${moment().format("YYYY-MM-DD")}, offset: 0, limit: 0) {
+        )}", endAt: "${moment().format("YYYY-MM-DD")}", offset: 0, limit: 0) {
             id
             user {
                 id
