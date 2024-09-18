ee8b8376cffc153627fe6a05db94338db086d6d9
ammarbror <ammarabror@gmail.com>
2024-09-18 23:02:36 +0700

    update

--- src/pages/UndianPage.tsx
+++ src/pages/UndianPage.tsx
@@ -121,7 +121,11 @@ export default function UndianPage({}: Props) {
       <div className="relative">
         <div style={containerStyle}></div>
         <div className="fixed inset-0 flex items-center justify-start">
-          <div className="text-black border-8 border-black p-8 rounded font-bold text-9xl mb-6 mx-10 tracking-wide text-center">
+          <div
+            className={`text-black ${
+              startRandom && "border-8 border-black p-8"
+            } rounded font-bold text-9xl mb-6 mx-10 tracking-wide text-center`}
+          >
             {/* <div
               className={`tracking-normal transition-all duration-1000 mb-4 ${
                 showConfetti ? "opacity-100" : "opacity-0"
